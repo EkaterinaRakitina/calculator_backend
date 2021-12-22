d8dbc73a6e1d744f884f13c12d2cc0261ae33a14
EkaterinaRakitina <erakitina.exceedteam@gmail.com>
2021-12-22 16:39:18 +0300

    created POST and DELETE methods route

--- src/modules/controllers/spend.controller.js
+++ src/modules/controllers/spend.controller.js
@@ -1,11 +1,34 @@
 const Spend = require('../../db/models/spend/index');
 
 module.exports.getAllSpends = (req, res, next) => {
-    Spend.find().then((result) => {
-        res.send({data: result});
-    });
+  Spend.find().then((result) => {
+    res.send({ data: result });
+  });
 };
 
 module.exports.createNewSpend = (req, res, next) => {
-    
-}
+  if (req.body.hasOwnProperty('text') && req.body.hasOwnProperty('num')) {
+    const spend = new Spend(req.body);
+    spend
+      .save()
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => console.log(err));
+  } else {
+    res.status(404).send('Not found');
+  }
+};
+
+module.exports.deleteSpend = (req, res, next) => {
+  if (req.query.id) {
+    Spend.deleteOne({ _id: req.query.id }).then((result) => {
+      Spend.find().then((result) => {
+        res.send(result);
+      });
+    })
+    .catch((err) => console.log(err));
+  }else {
+    res.status(404).send('Not found');
+  }
+};
--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const {
     getAllSpends,
     createNewSpend,
+    deleteSpend,
 } = require('../controllers/spend.controller');
 
 // Spends routes
 router.get('/allSpends', getAllSpends);
 router.post('/createSpend', createNewSpend);
+router.delete('/deleteSpend', deleteSpend);
 
 
 module.exports = router;
