c34b7ed48a7ff03bec4b3a5223403b4c43535d95
EkaterinaRakitina <erakitina.exceedteam@gmail.com>
2021-12-23 10:21:25 +0300

    create PATCH method rout

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -5,11 +5,13 @@ const {
   getAllSpends,
   createNewSpend,
   deleteSpend,
+  changeInfoSpend
 } = require('../controllers/spend.controller');
 
 // Spends routes
 router.get('/allSpends', getAllSpends);
 router.post('/createSpend', createNewSpend);
 router.delete('/deleteSpend', deleteSpend);
+router.patch('/changeSpend', changeInfoSpend);
 
 module.exports = router;
